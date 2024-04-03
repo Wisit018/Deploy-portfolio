3209f4204d2c07bb0ec9cddbad8f84552098baec
Wisit018 <lipton_pink@hotmail.co.th>
2024-04-03 16:00:43 +0700

    update version 2 :alien:
    update version 2 :alien:

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,68 +1,138 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
+import { DiJavascript1, DiReact, DiNodejs, DiSqllite } from "react-icons/di";
 import {
-  DiJavascript1,
-  DiReact,
-  DiNodejs,
-  DiMongodb,
-  DiPython,
-  DiGit,
-} from "react-icons/di";
-import { SiPostgresql, SiMysql  } from "react-icons/si";
+  SiPostgresql,
+  SiMysql,
+  SiTypescript,
+  SiNextdotjs,
+  SiMongodb,
+  SiBootstrap,
+  SiTailwindcss,
+  SiExpress ,
+} from "react-icons/si";
 import { TbSeo } from "react-icons/tb";
-import { FaHtml5, FaCss3Alt, FaWordpress  } from "react-icons/fa";
+import { FaHtml5, FaCss3Alt, FaWordpress, } from "react-icons/fa";
+import "./TechStack.css";
+import { TbBrandVite, TbBrandPython } from "react-icons/tb";
+
+
+
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <hr className="horizontal-line-top" />
       <Col xs={4} md={2} className="tech-icons">
         <FaHtml5 />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">Html</p>
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
         <FaCss3Alt />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">css</p>
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">JavaScript</p>
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
+        <SiTypescript />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">TypeScript ( basic )</p>
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">React</p>
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
+        <TbBrandVite />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">Vite</p>
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
-        <SiPostgresql />
+        <SiNextdotjs />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">Next js</p>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">Node js</p>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiExpress  />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">Express</p>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiBootstrap  />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">Bootstrap</p>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiTailwindcss   />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">Tailwind CSS</p>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMongodb />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">MongoDB</p>
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
         <SiMysql />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">MySQL</p>
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
+        <DiSqllite />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">SQLite</p>
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
+        <SiPostgresql />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">PostgreSQL</p>
+      </Col>
+
+
+
+      <Col xs={4} md={2} className="tech-icons">
+        <TbBrandPython />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">Python ( basic )</p>
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
         <FaWordpress />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">WordPress</p>
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
         <TbSeo />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">SEO WordPress </p>
       </Col>
 
-      
+
     </Row>
   );
 }
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -6,26 +6,139 @@ import {
   SiDiscord ,
   SiVercel,
   SiWindows,
+  SiGooglesheets,
+  SiGoogleforms,
+  SiGoogleappsscript,
+  SiGooglebard,
+  SiFigma,
+  SiPlesk ,
+  SiGoogleslides ,
+  SiDialogflow ,
 } from "react-icons/si";
+import "./Toolstack.css";
+import { IoNotificationsCircle } from "react-icons/io5";
+import { VscCopilot } from "react-icons/vsc";
+import { FaGithub,FaUbuntu  } from "react-icons/fa";
+
+
 
 function Toolstack() {
+  const logo = require('../../Assets/chatgpt.png'); 
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+        <hr className="horizontal-line-top" />
       <Col xs={4} md={2} className="tech-icons">
         <SiWindows  />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">Windows</p>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">Visual Studio Code</p>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <SiPostman />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">Postman</p>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <SiDiscord  />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">Discord</p>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
-        <SiVercel />
+        <SiVercel/>
+        <hr className="horizontal-line" />
+        <p className="techstack-text">Vercel</p>
       </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <FaGithub />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">GitHub</p>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiGooglesheets />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">Google Sheet <br></br> ( App Script )</p>
+        
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+      <SiGoogleforms />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">Google Form <br></br> ( App Script ) </p>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+      <SiGoogleslides  />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">Google Slide <br></br> ( App Script )</p>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+      <SiGoogleappsscript />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">Google Apps Script</p>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <IoNotificationsCircle />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">LINE Notify</p>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiDialogflow  />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">Dialogflow</p>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        {/* <img src="{logo_chatgpt}"></img> */}
+        <img src={logo} width={'70px'} height={'70px'} alt="chatgpt"/>
+        <hr className="horizontal-line" />
+        <p className="techstack-text">ChatGPT</p>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiGooglebard  />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">Bard | Gemini</p>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFigma />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">Figma</p>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <VscCopilot />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">GitHub Copilot</p>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPlesk  />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">Plesk Panel</p>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <FaUbuntu />
+        <hr className="horizontal-line" />
+        <p className="techstack-text">Ubuntu OS</p>
+      </Col>
+
+
+      
     </Row>
   );
 }
